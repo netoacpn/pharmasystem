8d9bca5eb318c60ec0e209dfa97a827fb817fd52
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-08-28 20:09:43 -0300

    :sparkles: feature: Adiciona ROUTE_PATH nas rotas públicas

--- src/pages/autorizacao/index.jsx
+++ src/pages/autorizacao/index.jsx
@@ -1,7 +1,18 @@
 import { Header } from "../../components/header/index.jsx"
 import { Container, Box, Typography } from "@mui/material"
+import { useNavigate } from "react-router-dom";
+import ROUTES_PATH from "../../routes/routesPath.js";
+import { useEffect } from "react";
 
 function SemAutorizacao(){
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate(ROUTES_PATH.inicio);
+    }, 3000);
+  }, []);
+
   return(
     <>
       <Header />
