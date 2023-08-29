5f1fec61cc7cad05cb4802326f65ae41150d48e9
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-08-29 20:12:32 -0300

    :bug: fix: Bug do deploy-context

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import { MedicamentosContextProvider } from './context/medicamentosContext.jsx';
-import { FarmaciasContextProvider } from './context/farmaciasContext.jsx';
+import { MedicamentosContextProvider } from './context/medicamentosContext';
+import { FarmaciasContextProvider } from './context/farmaciasContext';
 import { RoutesApp } from './routes'
 import { BrowserRouter } from 'react-router-dom'
 import { LocalStorageProvider } from './context/localStorageContext'
