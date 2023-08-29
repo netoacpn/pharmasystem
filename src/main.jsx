5f7956f5c2fa658091544f2c83ff4934471afb34
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-08-29 20:33:08 -0300

    :bug: fix: Retorna commit do bug do deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { MedicamentosContextProvider } from './context/MedicamentosContext.jsx'
-import { FarmaciasContextProvider } from './context/FarmaciasContext.jsx'
+import { FarmaciasContextProvider } from './context/farmaciasContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <MedicamentosContextProvider>
