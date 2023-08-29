bd3b713d41da5a965d3a4c14dd4e44bac75505d0
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-08-29 19:52:27 -0300

    :bug: fix: Tentativa de correção bug no deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import { MedicamentosContextProvider } from './context/medicamentosContext.jsx'
 import { FarmaciasContextProvider } from './context/farmaciasContext.jsx'
+import App from './App.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <MedicamentosContextProvider>
     <FarmaciasContextProvider>
       <App />
     </FarmaciasContextProvider>
-  </MedicamentosContextProvider>,
+  </MedicamentosContextProvider>
 )
