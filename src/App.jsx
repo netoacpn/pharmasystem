8eb57271bf305b7b3f6dc7ae25ece83da670689c
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-08-29 20:02:58 -0300

    :bug: fix: Teste de correção do bug do deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { MedicamentosContextProvider } from './context/medicamentosContext.jsx';
+import { FarmaciasContextProvider } from './context/farmaciasContext.jsx';
 import { RoutesApp } from './routes'
 import { BrowserRouter } from 'react-router-dom'
 import { LocalStorageProvider } from './context/localStorageContext'
@@ -6,11 +8,15 @@ import { LocalStorageProvider } from './context/localStorageContext'
 function App() {
 
   return (
-    <BrowserRouter>
-      <LocalStorageProvider>
-        <RoutesApp />
-      </ LocalStorageProvider>
-    </BrowserRouter>
+    <MedicamentosContextProvider>
+      <FarmaciasContextProvider>
+        <BrowserRouter>
+          <LocalStorageProvider>
+            <RoutesApp />
+          </ LocalStorageProvider>
+        </BrowserRouter>
+      </FarmaciasContextProvider>
+    </ MedicamentosContextProvider>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { MedicamentosContextProvider } from './context/medicamentosContext.jsx'
-import { FarmaciasContextProvider } from './context/farmaciasContext.jsx'
-import App from './App.jsx' 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <MedicamentosContextProvider>
-    <FarmaciasContextProvider>
-      <App />
-    </FarmaciasContextProvider>
-  </MedicamentosContextProvider>
-)
+  <App />
+);
